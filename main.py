8b631bc70e239d6d573b46c0c9868bbdb1bd2987
YooNa Chang <yc635@cornell.edu>
2019-09-21 14:08:32 -0400

    copied from toolkit+2

--- main.py
+++ main.py
@@ -1,82 +1,38 @@
-import os
-import sys
-
 from flask import Flask, redirect, render_template, request
 
-from google.cloud import firestore
-from google.cloud import storage
-from google.cloud import vision
+from google.cloud import language
+from google.cloud.language import enums
+from google.cloud.language import types
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def homepage():
-    # Create a Cloud Firestore client.
-    firestore_client = firestore.Client()
+    # Return a Jinja2 HTML template of the homepage.
+    return render_template('homepage.html')
 
-    # Use the Cloud Firestore client to fetch information from Cloud Firestore about
-    # each photo.
-    photo_documents = list(firestore_client.collection(u'photos').get())
+@app.route('/run_language', methods=['GET', 'POST'])
+def run_language():
+    # Create a Cloud Natural Language client
+    client = language.LanguageServiceClient()
 
-    # Return a Jinja2 HTML template.
-    return render_template('homepage.html', photo_documents=photo_documents)
+    # Retrieve inputted text from the form and create document object
+    text = request.form['text']
+    document = types.Document(content=text, type=enums.Document.Type.PLAIN_TEXT)
 
+    # Retrieve response from Natural Language API's analyze_entities() method
+    response = client.analyze_entities(document)
+    entities = response.entities
 
-@app.route('/upload_photo', methods=['GET', 'POST'])
-def upload_photo():
-    # Create a Cloud Storage client.
-    storage_client = storage.Client()
+    # Retrieve response from Natural Language API's analyze_sentiment() method
+    response = client.analyze_sentiment(document)
+    sentiment = response.document_sentiment
 
-    # Get the Cloud Storage bucket that the file will be uploaded to.
-    bucket = storage_client.get_bucket(os.environ.get('CLOUD_STORAGE_BUCKET'))
-
-    # Create a new blob and upload the file's content to Cloud Storage.
-    photo = request.files['file']
-    blob = bucket.blob(photo.filename)
-    blob.upload_from_string(
-        photo.read(), content_type=photo.content_type)
-
-    # Make the blob publicly viewable.
-    blob.make_public()
-    image_public_url = blob.public_url
-
-    # Create a Cloud Vision client.
-    vision_client = vision.ImageAnnotatorClient()
-
-    # Retrieve a Vision API response for the photo stored in Cloud Storage
-    image = vision.types.Image()
-    image.source.image_uri = 'gs://{}/{}'.format(os.environ.get('CLOUD_STORAGE_BUCKET'), blob.name)
-
-    response = vision_client.annotate_image({'image': image})
-    labels = response.label_annotations
-    faces = response.face_annotations
-    web_entities = response.web_detection.web_entities
-
-    # Create a Cloud Firestore client
-    firestore_client = firestore.Client()
-
-    # Get a reference to the document we will upload to
-    doc_ref = firestore_client.collection(u'photos').document(blob.name)
-
-    # Note: If we are using Python version 2, we need to convert
-    # our image URL to unicode to save it to Cloud Firestore properly.
-    if sys.version_info < (3, 0):
-        image_public_url = unicode(image_public_url, "utf-8")
-
-    # Construct key/value pairs with data
-    data = {
-        u'image_public_url': image_public_url,
-        u'top_label': labels[0].description
-    }
-
-    # Set the document with the data
-    doc_ref.set(data)
-
-    # Redirect to the home page.
-    return render_template('homepage.html', labels=labels, faces=faces, web_entities=web_entities,
-                           image_public_url=image_public_url)
-
+    # Return a Jinja2 HTML template of the homepage and pass the 'text', 'entities',
+    # and 'sentiment' variables to the frontend. These contain information retrieved
+    # from the Natural Language API.
+    return render_template('homepage.html', text=text, entities=entities, sentiment=sentiment)
 
 @app.errorhandler(500)
 def server_error(e):
