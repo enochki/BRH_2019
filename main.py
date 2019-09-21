a086e84f883ee4b5bc6ec415413ccc8cf8803506
YooNa Chang <yc635@cornell.edu>
2019-09-21 17:14:34 -0400

    score!!

--- main.py
+++ main.py
@@ -12,6 +12,18 @@ def homepage():
     # Return a Jinja2 HTML template of the homepage.
     return render_template('homepage.html')
 
+
+def score(annotations):
+    sum = 0.0
+    count = 0.0
+    for index, sentence in enumerate(annotations.sentences):
+        sentence_sentiment = sentence.sentiment.score
+        if (sentence_sentiment != 0.0):
+            sum = sum + (sentence_sentiment * sentence.sentiment.magnitude)
+            count += 1.0
+    return (sum / count)*10.0
+
+
 @app.route('/run_language', methods=['GET', 'POST'])
 def run_language():
     # Create a Cloud Natural Language client
@@ -21,18 +33,21 @@ def run_language():
     text = request.form['text']
     document = types.Document(content=text, type=enums.Document.Type.PLAIN_TEXT)
 
-    # Retrieve response from Natural Language API's analyze_entities() method
-    # response = client.analyze_entities(document)
-    # entities = response.entities
-
     # Retrieve response from Natural Language API's analyze_sentiment() method
     response = client.analyze_sentiment(document)
-    sentiment = response.document_sentiment
+    annotations = response.document_sentiment
+
+    sedcscore= score(annotations)
+
 
     # Return a Jinja2 HTML template of the homepage and pass the 'text', 'entities',
     # and 'sentiment' variables to the frontend. These contain information retrieved
     # from the Natural Language API.
-    return render_template('homepage.html', text=text, entities=entities, sentiment=sentiment)
+    return render_template('homepage.html', text=text, sentiment=sentiment)
+
+
+
+
 
 @app.errorhandler(500)
 def server_error(e):
